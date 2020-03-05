931163ee4b699a8e12b8ceb5afbd45b7387a7f55
Shawn Flesher <sflesh2@uic.edu>
2020-03-05 21:07:12 +0000

    added better player texture

--- game.js
+++ game.js
@@ -2,32 +2,84 @@ let player = {
     "x" : 0, 
     "y": 0, 
     "health": 100, 
-    "img" :  "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSZ7axdY00zeyQvGMRDBbMcIPwuVR_rT0R_wnjMnliXryAFLUsf",
+    "img" :  "./ship.png",
 };
-function clear(c, ctx){
+
+
+let playerImg = new Image(); 
+let c = document.querySelector("#gameCanvas");
+let ctx = c. getContext("2d");
+
+let moveLeft = false; 
+let moveRight = false; 
+let moveUp = false; 
+let moveDown = false; 
+
+
+function updateMove(){
+    if(moveLeft){
+        player["x"] -= 1; 
+    }else if(moveRight){
+        player["x"] += 1; 
+    }
+    
+    if(moveUp){
+        player["y"] -= 1; 
+    }else if(moveDown){
+        player["y"] += 1; 
+    }
+}
+
+playerImg.onload = ()=>{
+    player["x"] = c.width * .5; 
+    player["y"] = c.height * .9      
+}
+
+//set up the images for the player and the harm object and kick off the animation 
+function init(){
+    playerImg.src = player["img"];
+    window.requestAnimationFrame(draw);
+}
+
+function clear(){
    ctx.clearRect(0,0,c.width, c.height);
 }
+ 
+//when a key is released, the player stops moving
+document.addEventListener("keyup", e=>{
+    if(e.key == "a"){
+        moveLeft = false; 
+    }else if(e.key == "d"){
+        moveRight = false;
+    }else if(e.key == "w"){
+        moveUp = false; 
+    }else if(e.key == "s"){
+        moveDown = false;
+    }
+});
 
-var playerImg = new Image(); 
-var c = document.querySelector("#gameCanvas"); 
-var ctx = c.getContext("2d");
-var img = new Image()
-img.onload = ()=>{
-    player["x"] = c.width * .5; 
-    player["y"] = c.height * .9
-       
-}
-img.src = player["img"];  
+//starts moving the player
+document.addEventListener("keydown", e=>{
+    console.log(e);
+    if(e.key == "a"){
+        moveLeft = true; 
+    }else if(e.key == "d"){
+        moveRight = true; 
+    }else if(e.key == "w"){
+        moveUp = true;
+    }else if(e.key == "s"){
+        moveDown = true;
+    }
+});
 
 function draw(){
-   clearImage(c, ctx);
-   ctx.drawImage(img,player["x"],player["y"],img.naturalWidth*.25, img.naturalHeight*.25) 
-    
+   clear();
+   updateMove();
+   ctx.drawImage(playerImg,player["x"],player["y"],playerImg.naturalWidth, playerImg.naturalHeight) 
+   
    window.requestAnimationFrame(draw);
 }
 
-window.onload = function(){
-    window.requestAnimationFrame(draw);
-    
-}
+init();
+
 
